47b6dc735a738932954af9e84105106d5d1f08b1
tcruz <tcruz@corp.globo.com>
2020-04-30 16:56:35 -0300

    feat insert files in project

--- locut_test/scripts/locustfile.py
+++ locut_test/scripts/locustfile.py
@@ -1,24 +1,23 @@
-from locust import HttpLocust, TaskSet, task
-
+from locust import HttpLocust, TaskSet, task, between
+from locust.log import setup_logging
+from locust.contrib.fasthttp import FastHttpLocust
+import requests
 class PublicTaskSet(TaskSet):
-
     @task(1)
     def alternativa(self):
-        self.client.get('/experiments/premium-highlight/GLOBOPLAY-HOME-destaque-premium-com-recomendacao/:alternativa/')
-
+        self.client.get("/mve-api/globo-play-beta/experiments/premium-highlight/GLOBOPLAY-HOME-destaque-premium-com-recomendacao/multiplos-destaques-com-recomendacao/")
 class PrivateTaskSet(TaskSet):
-
     @task(1)
     def recommendation(self):
-        self.client.get('/experiments/premium-highlight/GLOBOPLAY-HOME-destaque-premium-com-recomendacao/recommendation?web=X1&tablet=X1,X1_5&tv=X1&mobile=X1,X1_5')
-
+       self.client.get("/mve-api/globo-play/experiments/premium-highlight/GLOBOPLAY-HOME-destaque-premium-com-recomendacao/recommendation?web=X1&mobile=X1",
+            headers = {
+                "authorization": "Bearer 1b36a0531234bf250529d5f21eaf6ccec4561495544634738424f725532582d7839554e4e575671426956704e6c45307a467267683731565948543073724575763653317958534e58317a6c7264724f664d367931437962364d5170425571624b66314a6771773d3d3a303a756b687776716f75726a6f78737578747077726e"
+            })
 class WebsiteTasks(TaskSet):
     tasks = {
         PublicTaskSet: 20,
         PrivateTaskSet: 10,
     }
-
-class BasicTasks(HttpLocust):
+class BasicTasks(FastHttpLocust):
     task_set = WebsiteTasks
-    min_wait = 5000
-    max_wait = 10000
+    wait_time = between(0.1, 1)
